d43fed6647df3dac003dcb017c295d9186119623
karkris41295 <karkris3@hotmail.com>
2016-06-25 00:03:58 +0530

    space time module complete.

--- working/module2_p/traffic.py
+++ working/module2_p/traffic.py
@@ -5,13 +5,13 @@ from matplotlib import rcParams
 rcParams['font.family'] = 'serif'
 rcParams['font.size'] = 16
 
-v_max = 80. #km/hr
+v_max = 136. #km/hr
 L = 11. #km
 rho_max = 250 #cars/km
 nx = 51
-nt = 30
 dx = L/(nx-1)
 dt = .001 #hrs
+nt = int(0.05/0.001)
 
 from sympy.utilities.lambdify import lambdify # converts expression into a funtio
 
@@ -20,24 +20,33 @@ flux = v_max*rho*(1-rho/rho_max)
 f_lamb = lambdify(rho, flux)
 
 x = numpy.linspace(0,L,nx)
-rho0 = numpy.ones(nx)*10
+rho0 = numpy.ones(nx)*20
 rho0[10:20] = 50
-rho0[0] = 20.
 
 fig = pyplot.figure(figsize=(11,8))
-ax = pyplot.axes(xlim=(0,11), ylim=(0,5000))
+ax = pyplot.axes(xlim=(0,11), ylim=(0,70))
 line = ax.plot([],[],color = '#003366', ls ='--',lw=3)[0]
 
-def traffic(i): 
-    
+for n in range(nt):
     rn = rho0.copy()
     rho0[0] = 20. 
     f = numpy.asarray([f_lamb(r0) for r0 in rho0])
-    line.set_data(x,f)
+    line.set_data(x,rho0)
+    rho0[1:] = rn[1:] -dt/dx*(f[1:] -f[0:-1])    
+
+#ANIMATION
+'''
+def traffic(i): 
+    
+    rn = rho0.copy()
+    rho0[0] = 10. 
+    f = numpy.asarray([f_lamb(r0) for r0 in rho0])
+    line.set_data(x,rho0)
     rho0[1:] = rn[1:] -dt/dx*(f[1:] -f[0:-1])
 
 from matplotlib import animation
 
 ani = animation.FuncAnimation(fig, traffic,interval=100)
+'''
 
 pyplot.show()  
