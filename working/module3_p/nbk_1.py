b95952448526756aef75012502c296c9c5586f87
karkris41295 <karkris3@hotmail.com>
2016-06-29 22:13:23 +0530

    add lesson 3

---
+++ working/module3_p/nbk_1.py
@@ -0,0 +1,153 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import rcParams
+rcParams['font.family'] = 'serif'
+rcParams['font.size'] = 16
+
+def rho_green_light(nx, rho_light):
+    """Computes "green light" initial condition with shock, and linear distribution behind
+
+    Parameters
+    ----------
+    nx        : int
+        Number of grid points in x
+    rho_light : float
+        Density of cars at stoplight
+
+    Returns
+    -------
+    rho: array of floats
+        Array with initial values of density
+    """    
+    rho = np.arange(nx) * 2./nx*rho_light
+    rho[int((nx-1)/2):] = 0
+    
+    return rho
+
+def computeF(u_max, rho_max, rho):
+    """Computes flux F=V*rho
+
+    Parameters
+    ----------
+    u_max  : float
+        Maximum allowed velocity
+    rho    : array of floats
+        Array with density of cars at every point x
+    rho_max: float
+        Maximum allowed car density
+        
+    Returns
+    -------
+    F : array
+        Array with flux at every point x
+    """
+    return u_max*rho*(1-rho/rho_max)
+    
+def ftbs(rho, nt, dt, dx, rho_max, u_max):
+    """ Computes the solution with forward in time, backward in space
+    
+    Parameters
+    ----------
+    rho    : array of floats
+            Density at current time-step
+    nt     : int
+            Number of time steps
+    dt     : float
+            Time-step size
+    dx     : float
+            Mesh spacing
+    rho_max: float
+            Maximum allowed car density
+    u_max  : float
+            Speed limit
+    
+    Returns
+    -------
+    rho_n : array of floats
+            Density after nt time steps at every point x
+    """
+    
+    #initialize our results array with dimensions nt by nx
+    rho_n = np.zeros((nt,len(rho)))      
+    #copy the initial u array into each row of our new array
+    rho_n[0,:] = rho.copy()              
+    
+    for t in range(1,nt):
+        F = computeF(u_max, rho_max, rho)
+        rho_n[t,1:] = rho[1:] - dt/dx*(F[1:]-F[:-1])
+        rho_n[t,0] = rho[0]
+        rho = rho_n[t].copy()
+
+    return rho_n
+'''
+#UNDER CONSTRUCTION
+def ftfs(rho, nt, dt, dx, rho_max, u_max):
+    """ Computes the solution with forward in time, forward in space
+    
+    Parameters
+    ----------
+    rho    : array of floats
+            Density at current time-step
+    nt     : int
+            Number of time steps
+    dt     : float
+            Time-step size
+    dx     : float
+            Mesh spacing
+    rho_max: float
+            Maximum allowed car density
+    u_max  : float
+            Speed limit
+    
+    Returns
+    -------
+    rho_n : array of floats
+            Density after nt time steps at every point x
+    """
+    
+    #initialize our results array with dimensions nt by nx
+    rho_n = np.zeros((nt,len(rho)))      
+    #copy the initial u array into each row of our new array
+    rho_n[0,:] = rho.copy()              
+    
+    for t in range(1,nt):
+        F = computeF(u_max, rho_max, rho)
+        rho_n[t,1:] = rho[1:] - dt/dx*(F[1:]-F[:-1])
+        rho_n[t,0] = rho[0]
+        rho = rho_n[t].copy()
+
+    return rho_n
+'''   
+
+#Basic initial condition parameters
+#defining grid size, time steps
+nx = 81
+nt = 40
+dx = 4.0/(nx-1)
+
+x = np.linspace(0,4,nx)
+
+rho_max = 10.
+u_max = 1.
+rho_light = 4.
+
+rho = rho_green_light(nx, rho_light)
+sigma = 1.
+dt = sigma*dx
+
+rho_n = ftbs(rho, nt, dt, dx, rho_max, u_max)
+
+from matplotlib import animation
+
+fig = plt.figure();
+ax = plt.axes(xlim=(0,4),ylim=(-.5,11.5),xlabel=('Distance'),ylabel=('Traffic density'));
+line, = ax.plot([],[],color='#003366', lw=2);
+
+def animate(data):
+    x = np.linspace(0,4,nx)
+    y = data
+    line.set_data(x,y)
+    return line,
+
+anim = animation.FuncAnimation(fig, animate, frames=rho_n, interval=50)
+plt.show()
